64b2794710422217f1529345c996a8e6805c5ee5
Jacob Phillips <jphilli85@gmail.com>
2012-12-08 16:58:24 -0500

    minor settings change

--- src/eecs6110/matrixmultiplication/devs/Settings.java
+++ src/eecs6110/matrixmultiplication/devs/Settings.java
@@ -16,19 +16,19 @@ class Settings {
     
     // If there are more slaves than rows in matrix1, the excess number
     // of slaves are ignored. Computer.java enforces a minimum of 1 and a
-    // maximum of 20.
+    // maximum of 20 slaves.
     static final int NUM_SLAVES             = 3;
 
     static final int MATRIX1_ROWS           = 100;
     static final int MATRIX1_COLS           = 100;
-    static final int MATRIX1_MIN_VALUE      = 0;
+    static final int MATRIX1_MIN_VALUE      = 1;
     static final int MATRIX1_MAX_VALUE      = 100;
     static final int MATRIX1_TYPE           = Matrix.TYPE_RANDOM;
     static final int MATRIX1_MODIFICATION   = MATRIX_MODIFICATION_NONE;    
     
     static final int MATRIX2_ROWS           = MATRIX1_COLS; // Don't change this one.
     static final int MATRIX2_COLS           = 100; 
-    static final int MATRIX2_MIN_VALUE      = 0;
+    static final int MATRIX2_MIN_VALUE      = 1;
     static final int MATRIX2_MAX_VALUE      = 100;
     static final int MATRIX2_TYPE           = Matrix.TYPE_RANDOM;
     static final int MATRIX2_MODIFICATION   = MATRIX_MODIFICATION_NONE;
